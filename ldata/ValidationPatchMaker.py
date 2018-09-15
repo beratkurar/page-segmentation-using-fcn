3fa5fc83e546e03dbaf81c9f3b80d08129059acc
beratkurar <beratkurar@gmail.com>
2018-09-15 11:03:53 +0300

    inital

---
+++ ldata/ValidationPatchMaker.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 30 07:43:28 2017
+
+@author: B
+"""
+import random
+import cv2
+import os
+
+patchSize=320
+patchNumber=0
+folder='validation/'
+lfolder='lvalidation/'
+i=0
+while (i <100):
+    pages=os.listdir(folder)
+    page_number=random.randint(0,3)
+    page_name=pages[page_number]
+    page=cv2.imread(folder+page_name,3)
+    lpage=cv2.imread(lfolder+page_name[:-3]+'bmp',0)
+    rows,cols,ch=page.shape
+    x=random.randint(0,rows-patchSize)
+    y=random.randint(0,cols-patchSize)    
+    lpatch=lpage[x:x+patchSize,y:y+patchSize] 
+    cv2.imwrite("p"+lfolder+page_name[:-4]+"_patch"+str(i)+".png",lpatch)
+    patch=page[x:x+patchSize,y:y+patchSize]
+    cv2.imwrite("p"+folder+page_name[:-4]+"_patch"+str(i)+".png",patch)
+    print(i)
+    i=i+1
+    
+
+    
+    
+    
+
